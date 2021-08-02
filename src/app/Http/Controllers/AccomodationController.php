56becef39a8df62b783a36627081b6574224f630
mitsumasaanan <mitsumasa.anan449@gmail.com>
2021-08-02 14:24:33 +0900

    ヘッダー（ログイン前）作成

---
+++ src/app/Http/Controllers/AccomodationController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AccomodationController extends Controller
+{
+    public function index()
+    {
+        return view('accomodations.index');
+    }
+}
---
+++ src/resources/views/commons/header.blade.php
@@ -0,0 +1,36 @@
+<header id="header">
+    <div id="app">
+        <nav class="navbar navbar-expand navbar-dark text-white mb-5 bg-info">
+            <a class="navbar-brand site-logo" href="">タビログ</a>
+            <ul class="navbar-nav ml-auto mr-3">
+
+                {{-- @if(Auth::check())
+
+                    <li class="nav-item">
+                        <a class="nav-link" href={{ route('articles.create') }}><i class="fas fa-pen mr-2"></i>投稿する</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('user.show') }}">マイページ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('logout') }}"
+                            onclick="event.preventDefault(); document.getElementById('logout').submit();">ログアウト</a>
+                        <form id="logout" method="POST" action="{{ route('logout') }}">
+                            @csrf
+                        </form>
+                    </li> --}}
+
+                {{-- @else --}}
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">ログイン</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
+                    </li>
+
+                {{-- @endif --}}
+            </ul>
+        </nav>
+    </div>
+</header>
