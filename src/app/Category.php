fd934f436ea71959bb8c12029d209ab02c9e3398
mitsumasaanan <mitsumasa.anan449@gmail.com>
2021-08-03 23:04:33 +0900

    Category.php修正（1対多の関係定義）

--- src/app/Category.php
+++ src/app/Category.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public function accomodations()
+    {
+        return $this->hasMany(Accomodation::class);
+    }
 }
