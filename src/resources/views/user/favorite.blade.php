20a07c069071b1e39fd6e8fa8cb247822d777d10
mitsumasaanan <mitsumasa.anan449@gmail.com>
2021-08-22 23:45:54 +0900

    見た目修正

--- src/resources/views/user/favorite.blade.php
+++ src/resources/views/user/favorite.blade.php
@@ -5,17 +5,15 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-9">
-            {{-- @include('user.myinfo') --}}
+            @include('user.myinfo')
             @include('user.tabs')
-            {{-- {{ $favorite_accomodations }} --}}
-                {{-- @php dd($favorite_accomodations) @endphp --}}
+            
                 @foreach($favorite_accomodations as $favorite_accomodation)
                 <div class="card mb-5">
                     <div class="card-body">
                         <div class="row">
                             <p class="col-md-4 text-md-right text-dark">タイトル</p>
                             <p class="col-md-6 text-dark">
-                                {{-- @php dd($favorite_accomodation) @endphp --}}
                                 {{ $favorite_accomodation->name }}
                             </p>
                         </div>
--- src/resources/views/user/tabs.blade.php
+++ src/resources/views/user/tabs.blade.php
@@ -3,6 +3,6 @@
         <a class="nav-link {{ Request::is('user') ? 'active' : '' }}" href="{{ route('user.show') }}">宿一覧</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link {{ Request::is('user/favorite') ? 'active' : '' }}" href="{{ route('user.favorite', ['id' => $auth->id]) }}">保存した宿</a>
+        <a class="nav-link {{ Request::is('user/'.$auth->id.'/favorite') ? 'active' : '' }}" href="{{ route('user.favorite', ['id' => $auth->id]) }}">保存した宿</a>
     </li>
 </ul>
