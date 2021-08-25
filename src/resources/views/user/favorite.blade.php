f6f320ef96be0c5dc76374458e1840f058b6d608
mitsumasaanan <mitsumasa.anan449@gmail.com>
2021-08-25 10:06:18 +0900

    マイページお気に入り一覧条件分岐追加

--- src/resources/views/user/favorite.blade.php
+++ src/resources/views/user/favorite.blade.php
@@ -7,25 +7,41 @@
         <div class="col-md-9">
             @include('user.myinfo')
             @include('user.tabs')
-            
-                @foreach($favorite_accomodations as $favorite_accomodation)
-                <div class="card mb-5">
-                    <div class="card-body">
-                        <div class="row">
-                            <p class="col-md-4 text-md-right text-dark">タイトル</p>
-                            <p class="col-md-6 text-dark">
-                                {{ $favorite_accomodation->name }}
-                            </p>
-                        </div>
-                        <div class="row">
-                            <p class="col-md-4 text-md-right text-dark">URL</p>
-                            <p class="col-md-6">
-                                <a href="{{ $favorite_accomodation->url }}" target="_blank" rel="noopener noreferrer">{{ $favorite_accomodation->url }}</a>
-                            </p>
+
+                @if($favorite_accomodations->isEmpty())
+
+                    <div class="card mb-5">
+                        <div class="card-body">
+                            <div class="row">
+                                <p class="col-md-6 text-dark">
+                                    保存した宿はありません。
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                @endforeach
+
+                @else
+
+                    @foreach($favorite_accomodations as $favorite_accomodation)
+                    <div class="card mb-5">
+                        <div class="card-body">
+                            <div class="row">
+                                <p class="col-md-4 text-md-right text-dark">タイトル</p>
+                                <p class="col-md-6 text-dark">
+                                    {{ $favorite_accomodation->name }}
+                                </p>
+                            </div>
+                            <div class="row">
+                                <p class="col-md-4 text-md-right text-dark">URL</p>
+                                <p class="col-md-6">
+                                    <a href="{{ $favorite_accomodation->url }}" target="_blank" rel="noopener noreferrer">{{ $favorite_accomodation->url }}</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+
+                @endif
         </div>
     </div>
 </div>
