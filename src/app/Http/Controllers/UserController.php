f6fce432240b29243b093242853ec9034fb96aab
mitsumasaanan <mitsumasa.anan449@gmail.com>
2021-10-04 22:27:51 +0900

    UserControllerのタイポ修正

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
-use App\accomodation;
+use App\Accomodation;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\UserRequest;
